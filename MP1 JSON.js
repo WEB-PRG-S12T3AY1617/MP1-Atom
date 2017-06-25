41b384293942440643e43446283fba2a6d7e9115
JBringas <jarold_bringas@dlsu.edu.ph>
2017-06-25 10:07:29 +0800

    update: json, Main, css

--- MP1 JSON.js
+++ MP1 JSON.js
@@ -1,6 +1,47 @@
-$.ajax({
-  url: root + '/posts/1',
-  method: 'GET'
-}).then(function(data) {
-  console.log(data);
+var root = 'https://jsonplaceholder.typicode.com';
+var if_empty = 0;
+
+$(document).ready(function(){
+    //user_but clicked
+    $("#user_but").click(function() {
+      $("#section_header").text("Users");
+      retUser();
+  });
+  //retrieves users
+  function retUser() {
+    if(if_empty !== 0) {
+      clearCont();
+    }
+    var n = 0;
+    var reqUser = new XMLHttpRequest();
+    reqUser.open('GET', root + '/users');
+    reqUser.onload = function(){
+      var userNme = JSON.parse(reqUser.responseText);
+      while(n < userNme.length) {
+        $("#post_1").append(userNme[n].name + "</br>");
+        n = n + 1;
+      };
+    };
+    reqUser.send();
+    if_empty = 1;
+  };
+  //post_but clicked
+  $("#post_but").click(function () {
+      $("#section_header").text("Posts");
+      retPost();
+  });
+  //retrieves post
+  function retPost() {
+    alert('post section');
+    if(if_empty !== 0) {
+      clearCont();
+    }
+    // create new div for post
+  };
+  //clears contents of container_div
+  function clearCont() {
+    $("#post_1").text("");
+    alert('now empty');
+  };
+
 });
